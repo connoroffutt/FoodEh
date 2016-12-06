917024c51312c12da0216ac96d5d4c482ac7809a
Andrea <andrea.africa93@gmail.com>
2016-12-06 14:33:19 -0500

    Added cuisine index view page and seeded cuisine info

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "cusines#index"
+  root "cuisines#index"
 
   resources :cuisines, only: [:index, :show]
   resources :users, only: [:new, :create, :show]
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
-  
+
   post '/list/:id/add' => 'lists#add'
 
 
