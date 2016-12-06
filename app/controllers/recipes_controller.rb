55cde06a290edf563c0263a2cc962c40357635b6
Andrea <andrea.africa93@gmail.com>
2016-12-06 17:33:27 -0500

    search tag for recipes semi implemented

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,12 @@
 class RecipesController < ApplicationController
 
   def index
-    #code
+
+    # @recipes = Recipe.search(params[:search])
+
+    @search_term = params[:looking_for] || 'chocolate'
+    @recipes = Recipe.for(@search_term)
+
   end
 
   def show
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,19 @@ class Recipe < ApplicationRecord
   belongs_to :users
   has_many :users
 
+  include HTTParty
+  ENV["FOOD2FORK_KEY"] = "5a73407284d9736f03d4d5d6a193a04d"
+  base_uri 'http://food2fork.com/api'
+  default_params key: ENV["FOOD2FORK_KEY"]
+  format :json
+
+  def self.for term
+    get("/search", query: {q: term})["recipes"]
+  end
+
+  # def self.search(search)
+  #   if search
+  #     find(:all, :conditions => ['name LIKE ?', '%#{search}%'])
+  #   end
+  # end
 end
