d1692fe44eb1b4b8e17a4976247b171a07aad641
jasmine <93n.jas@gmail.com>
2016-12-08 16:50:52 -0500

    create f2fapi_helper, now we can use the api on diff model

---
+++ app/helpers/f2fapi_helper.rb
@@ -0,0 +1,10 @@
+module F2fapiHelper
+
+  include HTTParty
+
+  ENV["FOOD2FORK_KEY"] = "5a73407284d9736f03d4d5d6a193a04d"
+  base_uri 'http://food2fork.com/api'
+  default_params key: ENV["FOOD2FORK_KEY"]
+  format :json
+  
+end
