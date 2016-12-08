aa90941e5d8fecb70b235aa0b91e3e1a00dbfb2c
Andrea <andrea.africa93@gmail.com>
2016-12-08 15:42:40 -0500

    dec8 pulling latest version pt3

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,8 +8,8 @@ class RecipesController < ApplicationController
     # @recipes = Recipe.search(params[:search])
 
 
-    @get_term = params[:looking_for] || 'chicken'
-    @recipes = Recipe.for(@get_term)
+    # @get_term = params[:looking_for] || 'chicken'
+    # @recipes = Recipe.for(@get_term)
 
     @get_term = params[:search]
       if @get_term == nil
@@ -17,28 +17,15 @@ class RecipesController < ApplicationController
       elsif @get_term.empty?
         @recipes == []
       else
-<<<<<<< HEAD
-    @recipes = Recipe.for(@get_term)
-      end
-=======
         @recipes = Recipe.for(@get_term)
       end
-
-  end
->>>>>>> 955b96c0ca6027594f4a6e0be515e8d73b2236cc
 
   end
 
   def show
     @get_id = params[:id]
     @recipes = Recipe.get_recipe(@get_id)
-<<<<<<< HEAD
-    
-    # @get_id = params[:get]
-    # @recipes = Recipe.for(@get_id)
-=======
 
->>>>>>> 955b96c0ca6027594f4a6e0be515e8d73b2236cc
   end
 
 
