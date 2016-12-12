379fbcfdff271a97c163b64137ba8cffe51b35fd
connor <connor.ferguson.offutt@gmail.com>
2016-12-12 17:01:03 -0500

    recipe show page fixed to show data from our own database not the APIs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212204325) do
+ActiveRecord::Schema.define(version: 20161212210304) do
 
   create_table "cuisines", force: :cascade do |t|
     t.integer  "cuisine_id"
