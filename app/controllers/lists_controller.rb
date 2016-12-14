671c408f723aea116b68d97ae2c5831e6b31d40d
jasmine <93n.jas@gmail.com>
2016-12-14 14:28:56 -0500

    ingredients are now showing up on the user show page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,5 +2,7 @@ class ListsController < ApplicationController
   def add
     list = List.find_or_create_by(user: current_user)
     list.ingredients << Ingredient.find(params[:ingredient][:ingredient_id])
+
+    redirect_to(user_path(current_user))
   end
 end
