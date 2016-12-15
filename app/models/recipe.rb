45f0fd5efca22acb186d71a477c838d2396cdb0d
Andrea <andrea.africa93@gmail.com>
2016-12-15 17:55:39 -0500

    Added video to Cuisine index page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -15,7 +15,7 @@ class Recipe < ApplicationRecord
 
   def self.get_recipe(id)
     #  data = get("/get", query: {rId: id})["recipe"]
-    data = JSON.parse(File.read("#{Rails.root}/public/#{id}.json"))["recipes"]
+    data = JSON.parse(File.read("#{Rails.root}/public/#{id}.json"))["recipe"]
     recipe = Recipe.create!(f2f_id: data["recipe_id"], name: data["title"])
     data["ingredients"].each do |i|
       recipe.ingredients.create!(description: i)
