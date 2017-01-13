f12fb1959cf95aaf63bf265714c24c7787d0fb06
Andrea <andrea.africa93@gmail.com>
2017-01-13 15:50:11 -0500

    pushing temporary styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,5 @@
 //= require_tree .
 
 $(document).ready(function(){
-  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal-trigger').leanModal();
 });
